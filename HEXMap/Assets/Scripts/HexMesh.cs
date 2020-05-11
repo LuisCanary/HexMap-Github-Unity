cd33650a867328a2634dee5cb1f3150b3addcd51
Luis <luisgomezruiz@gmail.com>
2020-05-11 19:03:35 +0200

    Colors completed, next blending

--- HEXMap/Assets/Scripts/HexMesh.cs
+++ HEXMap/Assets/Scripts/HexMesh.cs
@@ -38,11 +38,7 @@ public class HexMesh : MonoBehaviour {
 	void Triangulate (HexCell cell) {
 		Vector3 center = cell.transform.localPosition;
 		for (int i = 0; i < 6; i++) {
-			AddTriangle(
-				center,
-				center + HexMetrics.corners[i],
-				center + HexMetrics.corners[i + 1]
-			);
+			AddTriangle(center,center + HexMetrics.corners[i],center + HexMetrics.corners[i + 1]);
 			AddTriangleColor(cell.color);
 		}
 	}
